c541925a9dccb421535b0c922d56d402cd4db36a
ArtemRodionov <ostin161@gmail.com>
2026-02-24 00:31:31 +0300

    feat(UserSettings): Добавлен менеджер настроек пользователя.

---
+++ echos/Utilities/UserSettings.swift
@@ -0,0 +1,38 @@
+//
+//  UserSettings.swift
+//  echos
+//
+//  Created by Artem Rodionov on 24.02.2026.
+//
+
+import Foundation
+
+enum UserSettings {
+    
+    private static var userNameKey = "echos_user_name"
+    
+    static var userName: String? {
+        get {
+            UserDefaults.standard.string(forKey: userNameKey)
+        }
+        set {
+            UserDefaults.standard.set(newValue, forKey: userNameKey)
+        }
+    }
+    
+    static var hasCompletedOnboarding: Bool {
+        userName != nil
+    }
+    
+    static var displayName: String {
+        if let customName = userName, !customName.isEmpty {
+            return customName
+        }
+        
+        return DeviceInfo.deviceName
+    }
+    /// Сбросить настройки 
+    static func rest() {
+        UserDefaults.standard.removeObject(forKey: userNameKey)
+    }
+}
